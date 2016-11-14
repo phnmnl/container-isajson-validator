43a1626cddf8e9e945c97f6f12a16a32574eba22
David Johnson <david.johnson@oerc.ox.ac.uk>
2016-11-14 17:13:21 +0000

    Add test and modify container a bit

---
+++ run_test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+apt-get update && apt-get install -y --no-install-recommends wget
+wget --no-check-certificate https://raw.githubusercontent.com/ISA-tools/isa-api/master/tests/data/json/BII-I-1/BII-I-1.json
+mkdir /tmp/
+python3 run_validator.py BII-I-1.json
+
+# check that files were created
+if ! [ -e "/tmp/report.json" ]; then
+	echo "Validation report file doesn't exist"
+	exit 1
+fi
+
+echo "All files created successfully"
--- run_validator.py
+++ run_validator.py
@@ -1,11 +1,12 @@
-import sys
+import sys, json
 
 isa_json_file = sys.argv[1]
 try:
     from isatools import isajson
 except ImportError as e:
     raise RuntimeError("Could not import isatools package")
-log_msg_stream = isajson.validate(open(isa_json_file))
-if log_msg_stream is not None:
-    with open('log.txt', 'w') as f:
-        f.write(log_msg_stream.getvalue())
+with open(isa_json_file) as in_fp:
+    json_report = isajson.validate(in_fp)
+    if json_report is not None:
+        with open('report.json', 'w') as out_fp:
+           json.dump(json_report, out_fp)
